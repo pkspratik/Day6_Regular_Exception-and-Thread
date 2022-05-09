1b94e515e51164a00050435d936e64c58104d579
pkspratik <52348344+pkspratik@users.noreply.github.com>
2022-05-09 17:43:29 +0530

    Delete Newprogram directory

--- Newprogram/src/com/pks/test/p/$.java
+++
@@ -1,8 +0,0 @@
-package com.pks.test.p;
-
-public class $ {
-public static void main(String args[])
-{
-System.out.println("This is class Name is  $ ");	
-}
-}
--- Newprogram/src/com/pks/test/p/_123.java
+++
@@ -1,8 +0,0 @@
-package com.pks.test.p;
-
-public class _123 {
-public static void main(String a[])
-{
-	System.out.println("_ class name ");
-}
-}
